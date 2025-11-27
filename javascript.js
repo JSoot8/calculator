80d45929274fb389780245e3ccd722c24cb11882
JSoot8 <JSoot8@gmail.com>
2025-11-27 08:03:50 +0200

    Add core logic for all calculator buttons
    This commit adds the primary event handler for button clicks, enabling:
    - Number entry for num1 and num2.
    - Operator selection, including chaining operations (e.g., 2+2+).
    - Equals (=) functionality to execute the calculation.
    - All Clear (AC) functionality to reset state.

--- javascript.js
+++ javascript.js
@@ -3,6 +3,8 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 const operate = (a, b, operator) => {
+    a = parseFloat(a);
+    b = parseFloat(b);
     switch(operator) {
         case '+':
             return sum(a, b);
@@ -10,14 +12,70 @@ const operate = (a, b, operator) => {
         case '-':
             return subtract(a, b);
 
-        case '*':
+        case 'x':
             return multiply(a, b);
 
-        case '/':
+        case '÷':
             return divide(a, b);
     }
 };
 
-let num1;
-let num2;
-let operator;
+let num1 = '';
+let num2 = '';
+let operator = '';
+let operatorUsed = false;
+let newCalc = true;
+
+const mathOperatorArr = ['+', '-', 'x', '÷'];
+
+const btns = document.querySelector('.input-area');
+const display = document.querySelector('.output-area');
+
+btns.addEventListener('click', (event) => {
+    const clickedContent = event.target.textContent;
+    if(clickedContent === 'AC'){
+        num1 = '';
+        num2 = '';
+        operator = '';
+        operatorUsed = false;
+    }
+
+    else if(clickedContent === '='){
+        //no need to add the operator here too because num2 only gets values after the operator gets them first.
+        if(num1 !== '' && num2 !== '')
+        {
+            num1 = operate(num1, num2, operator);
+            num2 = '';
+            operator = '';
+            operatorUsed = false;
+            newCalc = false;
+        }
+    }
+    else if(mathOperatorArr.includes(clickedContent)){
+        if(operatorUsed === true && num2 !== ''){
+            num1 = operate(num1, num2, operator);
+            num2 = '';
+        }
+        operator = clickedContent;
+        operatorUsed = true;
+    }
+    //only insert the 2nd num after an operator has been used.
+    else if(operatorUsed === true){
+        num2 += clickedContent;
+    }
+    else {
+        if(newCalc === true){
+            num1 += clickedContent;
+        }
+        else {
+            num1 = clickedContent;
+            newCalc = true;
+        }
+    }
+    if(num1 === Infinity){
+        display.firstElementChild.textContent = 'STOP THIS NONSENSE'
+    }
+    else {
+        display.firstElementChild.textContent = `${num1} ${operator} ${num2}`;
+    }
+}); 
